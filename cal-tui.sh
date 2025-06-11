096a12cf19e845f51ba2c2c8de6f39f10b3dfd62
Chris Cowan <calahilstudio@gmail.com>
2025-06-10 22:29:07 -0700

    2nd

--- cal-tui.sh
+++ cal-tui.sh
@@ -121,9 +121,11 @@ cal-tui::main_menu() {
                 echo "Goodbye!"
                 exit 0
             fi
-            eval "${commands[$((choice - 1))]}"
-            echo -e "\nPress Enter to return to menu..."
-            read -r
+            (
+                eval "${commands[$((choice - 1))]}"
+                echo -e "\nPress Enter to return to menu..."
+                read -r
+            )
         else
             cal-tui::print_error "Invalid choice. Try again."
             sleep 1
